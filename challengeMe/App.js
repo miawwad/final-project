96cab4ad7509970b5acad90105a8e795bd5ab31a
Ayham-Kamal <ayhamk2017@gmail.com>
2024-04-02 01:16:04 -0400

    Added username and password check, edited small bugs

--- challengeMe/App.js
+++ challengeMe/App.js
@@ -16,7 +16,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // Just edited the style.
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
