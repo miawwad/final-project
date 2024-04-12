92395bf86e1334138185dafe3fc4789688ac8b3d
Ayham-Kamal <ayhamk2017@gmail.com>
2024-04-12 10:24:49 -0400

    Added A comment

--- challengeMe/mainPage.js
+++ challengeMe/mainPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ScrollView, Text, TouchableOpacity, StyleSheet, Dimensions, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'; // Adjusted to use @expo/vector-icons if necessary
 
-const categories = Object.keys(categoryIcons);
+const categories = Object.keys(categoryIcons); // Comment
 
 const MainScreen = ({ navigation }) => {
   return (
