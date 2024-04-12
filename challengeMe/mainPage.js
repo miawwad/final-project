ce73f02211f05fc9c31a33bf297e1fab7ac95e5a
Ayham-Kamal <ayhamk2017@gmail.com>
2024-04-12 10:38:51 -0400

    Created the mainPage file, and added a template for JS (Logic), JSX (Component), CSS (Style).

---
+++ challengeMe/mainPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ScrollView, Text, TouchableOpacity, StyleSheet, Dimensions, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Adjusted to use @expo/vector-icons if necessary
+
+const categories = Object.keys(categoryIcons);
+
+const MainScreen = ({ navigation }) => {
+  return (
+    <p>hI,....</p>// This is just a template.  
+  );
+};
+
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  
+});
+
+export default MainScreen;
